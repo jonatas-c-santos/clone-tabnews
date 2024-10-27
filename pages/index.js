9a30c93c0c39a517e95a19217dc848f55c51992f
Jonatas C. Santos <150307670+jonatas-c-santos@users.noreply.github.com>
2024-10-27 00:40:53 +0000

    run

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Se você pensa que pode ou se pensa que não pode, de qualque forma, você está certo.</h1>
+    return <h1>Se você pensa que pode ou se pensa que não pode, de qualquer forma, você está certo.</h1>
     
 }
 
